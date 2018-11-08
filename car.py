1cfaf98f3d8d11c7e0c208d5f594a4bc0a46aac9
armostafizi <mostafia@oregonstate.edu>
2018-11-08 10:13:06 -0800

    Add class of Cars

---
+++ car.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+# code includes tha class of cars
+
+class Car:
+    def __init__(self, origin, destination):
+        self.origin = origin
+        self.destination = destination
+
