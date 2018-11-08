85e82ff6291a029f29004420e3c5afd8563d0a8a
armostafizi <mostafia@oregonstate.edu>
2018-11-08 13:17:15 -0800

    clean up and organize controller.py

--- controller.py
+++ controller.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python
-
 # This file controls Netlogo from Python
 
 
-if __name__ == '__main__':
-    import pyNetLogo
-    import sys
-    from random import randrange
-    from car import *
-    
-    GRID_SIZE = 5
-    NUM_CARS = 100
-    netlogo = pyNetLogo.NetLogoLink(gui=True,
-                                    netlogo_home = '/home/alire/app/netlogo-5.3-64/app/',
-                                                    #path to Netlogo installation (jar files,
-                                                    #note the "/app")
-                                    netlogo_version = '5')
-                                                      #netlogo version, either '5' or '6'
-    netlogo.load_model('/home/alire/mas/project/autonomous_routing/mars.nlogo')
-                        #path to the model
-
-    netlogo.command('set grid-size %d' % (GRID_SIZE))
-    #netlogo.command('set num-cars %d' % (NUM_CARS))
-    netlogo.command('setup')
-    
-    # create cars
+# create cars, assigns random routes and finishes up the setup
+def create_cars(n): # takes the number of cars
     cars = []
-    for i in range(NUM_CARS):
+    for i in range(n):
         origin = Intersection(0, randrange(GRID_SIZE))
         destination = Intersection(GRID_SIZE - 1, randrange(GRID_SIZE))
         c = Car(origin, destination)
@@ -35,11 +14,50 @@ if __name__ == '__main__':
         netlogo.command('setup-car-python %d %d %s' % (c.origin.y,
                                                        c.destination.y,
                                                        c.get_route_str()))
-
+    # Finish the setup
     netlogo.command('finish-setup')
+    return cars
 
-    x = raw_input()
+# opens up the model and initialize
+def fire_up(s): # takes the size of the grid
+    netlogo = pyNetLogo.NetLogoLink(gui=True,
+                                    netlogo_home = '/home/alire/app/netlogo-5.3-64/app/',
+                                                    #path to Netlogo installation (jar files,
+                                                    #note the "/app")
+                                    netlogo_version = '5')
+                                                      #netlogo version, either '5' or '6'
+    netlogo.load_model('/home/alire/mas/project/autonomous_routing/mars.nlogo')
+                        #path to the model
+    
+    # adjusts the grid size
+    netlogo.command('set grid-size %d' % (s))
+
+    # setup the grid, origins, and destinations
+    netlogo.command('setup')
+    return netlogo
+
+if __name__ == '__main__':
+    import pyNetLogo
+    import sys
+    from random import randrange
+    from car import *
+    
+    # globals
+    GRID_SIZE = 5
+    NUM_CARS = 100
+
+    # Fire up the model
+    netlogo = fire_up(GRID_SIZE)
+
+    # create cars and assign random routes
+    cars = create_cars(NUM_CARS)
+    
+    # Run the procedure
+    try:
+        while netlogo.report('ticks') < 1000:
+            netlogo.command('go')
+    except KeyboardInterrupt:
+        pass
+
     netlogo.kill_workspace()
-    #while netlogo.report('ticks') < 1000:
-    #    netlogo.command('go')
 
